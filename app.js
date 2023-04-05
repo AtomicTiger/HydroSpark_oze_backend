7f19b6a0a762846d91b6bd51190eb8e8f4d6914a
unknown <200417pepe@gmail.com>
2023-04-05 08:30:13 +0200

    make func to conect with mongose data base

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+// Connection URI
+const uri = 'mongodb://localhost:27017/myDatabase';
+
+// Connect to MongoDB using Mongoose
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected successfully to MongoDB');
+})
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB:', err);
+    process.exit(1);
+});
 
 const app = express();
 const port = 3000;
 
+
+
+
 app.get('/', (req,res) => {
     res.send('hello hydrospark squad')
+    res.send(main())
 })
 
 app.listen(port, ()=>{
