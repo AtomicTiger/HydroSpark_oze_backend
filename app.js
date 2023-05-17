0da3f466e31365c3d6cc4d00c162dd293a62e8ec
Piotr Kwasniowski <kwasniowskipiotr5@gmail.com>
2023-05-17 19:56:31 +0200

    changed corsa for oficial hydrospakr website domein

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const port = 3000;
 app.use(cors());
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://hydrospark.onrender.com/',
     optionsSuccessStatus: 200,
     allowedHeaders: 'Content-Type, Authorization'
 }));
